6302858593aced1756ee75692fc25d860ec58f5b
Vidushani <108521900+dvnishshanka@users.noreply.github.com>
2023-02-14 00:07:36 +0100

    Add Cart Modal and Backdrop

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/common/header/Header';
 import Meals from './components/common/meals/Meals';
+import Cart from './components/common/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
+  const [showCartItem, setShowCartItem] = useState(false);
+
+  const showCartHandler = () => {
+    setShowCartItem(true);
+  };
+
+  const closeCartHandler = () => {
+    setShowCartItem(false);
+  };
+
   return (
-    <React.Fragment>
-      <Header />
+    <CartProvider>
+      {showCartItem && <Cart onClose={closeCartHandler} />}
+      <Header onShowCartHandler={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
--- src/components/common/header/HeaderCartButton.js
+++ src/components/common/header/HeaderCartButton.js
@@ -1,14 +1,21 @@
 import classes from './HeaderCartButton.module.css';
 import CartIcon from './CartIcon';
+import { useContext } from 'react';
+import CartContext from './../../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const noOfItems = cartCtx.items.reduce((accumulator, item) => {
+    return accumulator + item.amount;
+  }, 0);
+
   return (
-    <button className={classes.headerCartButton}>
+    <button className={classes.headerCartButton} onClick={props.onClick}>
       <span className={classes.cartImage}>
         <CartIcon />
       </span>
       <span> Your Cart </span>
-      <span className={classes['badge']}>5</span>
+      <span className={classes['badge']}>{noOfItems}</span>
     </button>
   );
 };
